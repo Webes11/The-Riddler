a2d0bc1d538c83c0adcea6bb1ea16c9447461ca5
Webes11 <69060638+Webes11@users.noreply.github.com>
2021-07-12 09:02:34 -0700

    Create RiddlerExpress_2021-7-9.py

---
+++ RiddlerExpress_2021-7-9.py
@@ -0,0 +1,38 @@
+values = []
+# Test every possible value for A
+for A in range(1, 21, 1):
+    # Reject any even values
+    if A % 2 == 0: 
+        continue
+    # Test every possible value for B
+    for B in range(1, 21, 1): 
+        if B % 2 == 0:
+            continue
+        TmpA = A
+        TmpB = B
+        max_years = 0
+		# Keep adding A to TmpA and B to TmpB until they are within 1 of each other
+        while True:
+            if TmpA - TmpB == 1 or TmpA - TmpB == 0:
+                max_years = TmpA
+                break
+            elif TmpA - TmpB == -1:
+                max_years = TmpB
+                break
+            elif TmpA > TmpB:
+                TmpB += B
+            elif TmpA < TmpB:
+                TmpA += A
+        values.append((f"A: {A}, B: {B}", max_years))
+		
+# Find the largest value in the list
+max_years = 0
+for item in values:
+    if item[1] > max_years:
+        max_years = item[1]
+print(max_years)
+
+
+
+
+
